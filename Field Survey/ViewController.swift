5f7ef84162f01df462a936df0a0bf1e590c624dc
Jingsong Tan <jtnfx@mail.missouri.edu>
2020-07-24 14:44:38 -0500

    Initial Commit

---
+++ Field Survey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Field Survey
+//
+//  Created by Tan Jingsong on 7/24/20.
+//  Copyright © 2020 Tan Jingsong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
